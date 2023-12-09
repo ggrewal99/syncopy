a8145c822bf9fbf9ff5a9a38e5ebece4a50ebd42
ggrewal99 <guruvindergrewal98@gmail.com>
2023-12-09 21:09:23 +1100

    Make layout responsive and add view more button logic

--- script.js
+++ script.js
@@ -15,12 +15,13 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     };
 
-
-
     const carouselBtns = document.querySelectorAll(".carousel-btn");
     const primaryHeading = document.querySelector(".primary-heading");
     const lg_poster = document.querySelector(".lg-poster-container img");
-    const dropdown = document.querySelector("#language");
+    const sm_posters = document.querySelectorAll(".sm_poster");
+    const viewMoreBtn = document.querySelector(".view-more-btn");
+    let viewMoreClicked = false;
+
     carouselBtns.forEach(btn => {
         btn.addEventListener("click", () => {
             carouselBtns.forEach(otherBtn => otherBtn.classList.remove("active"));
@@ -31,6 +32,28 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    viewMoreBtn.addEventListener("click", () => {
 
+        sm_posters.forEach(poster => {
+            poster.style.display = "block";
+            viewMoreBtn.style.display = "none";
+        });
+        viewMoreClicked = true;
+    });
 
+    window.addEventListener('resize', () => {
+        if (viewMoreClicked) {
+            return;
+        }
+        let windowWidth = window.innerWidth;
+
+        if (windowWidth < 450) {
+            sm_posters.forEach((poster, index) => {
+                if (index != 0) {
+                    poster.style.display = "none";
+                    viewMoreBtn.style.display = "block";
+                }
+            });
+        }
+    });
 });
